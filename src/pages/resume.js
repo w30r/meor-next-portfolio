260641a9ee886f9ebf72002929cee6b68d617710
w30r <mr.meorsyamil@gmail.com>
2023-04-05 02:49:47 +0800

    maybe that?

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { motion } from "framer-motion";
-import TealCircle from "./tealcircle";
 
 export default function Kereta() {
   const [data, setData] = useState("");
