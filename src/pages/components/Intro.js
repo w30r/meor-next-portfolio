aae1d3566f986c9fdef5f4be1c48634e49d0c1c4
w30r <mr.meorsyamil@gmail.com>
2023-04-06 17:22:12 +0800

    icon

--- src/pages/components/Intro.js
+++ src/pages/components/Intro.js
@@ -17,10 +17,25 @@ function Intro() {
       </p>
       <button
         type="button"
-        class="text-white mt-6 font-sans bg-teal-700 hover:bg-teal-800 focus:outline-none focus:ring-4  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-teal-600 dark:hover:bg-teal-700 "
+        class="text-white mt-6 mb-24 font-sans bg-teal-700 hover:bg-teal-800 focus:outline-none focus:ring-4  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 dark:bg-teal-600 dark:hover:bg-teal-700 "
       >
         Hire me!
       </button>
+      <svg
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        className="w-6 h-6 mx-auto"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
+        ></path>
+      </svg>
     </div>
   );
 }
--- src/pages/components/Skills.js
+++ src/pages/components/Skills.js
@@ -3,29 +3,41 @@ import React from "react";
 function Skills() {
   return (
     <div className="bg-teal-900 w-screen m-6 py-4">
-      <h1 className="mt-4 mb-2 text-xl font-semibold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">
+      <h1 className="mt-4 mb-6 text-xl font-semibold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">
         Skillset
       </h1>
       <div className="font-semibold text-xs flex flex-wrap justify-center items-center">
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-visible text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
           <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             TypeScript
           </span>
+          <div class="absolute inline-flex items-center justify-center w-12 h-6 text-xs font-light text-white bg-cyan-700 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+            80%
+          </div>
         </button>
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-visible text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
           <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Python
           </span>
+          <div class="absolute inline-flex items-center justify-center w-12 h-6 text-xs font-light text-white bg-cyan-700 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+            85%
+          </div>
         </button>
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-visible text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
           <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             JavaScript
           </span>
+          <div class="absolute inline-flex items-center justify-center w-12 h-6 text-xs font-light text-white bg-cyan-700 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+            95%
+          </div>
         </button>
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-visible text-sm font-light  text-gray-900 rounded-lg group bg-gradient-to-br from-teal-600 to-blue-500 group-hover:from-teal-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
           <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             RStudio
           </span>
+          <div class="absolute inline-flex items-center justify-center w-12 h-6 text-xs font-light text-white bg-cyan-900 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+            30%
+          </div>
         </button>
       </div>
     </div>
