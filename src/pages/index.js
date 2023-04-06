dfe31a59c9fd32035a178db282a04dee57d28b61
w30r <mr.meorsyamil@gmail.com>
2023-04-06 17:00:01 +0800

    index.js: class to className

--- src/pages/index.js
+++ src/pages/index.js
@@ -102,32 +102,35 @@ export default function Home() {
         </main>
       </html>
 
-      <footer class=" px-4 bg-white rounded-lg shadow m-6 dark:bg-black">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span class="text-sm text-teal-700 sm:text-center dark:text-teal-700">
+      <footer className=" px-4 bg-white rounded-lg shadow m-6 dark:bg-black">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-teal-700 sm:text-center dark:text-teal-700">
             © 2023{" "}
-            <a href="https://google.com/" class="hover:underline font-semibold">
+            <a
+              href="https://google.com/"
+              className="hover:underline font-semibold"
+            >
               developedbymeor
             </a>
           </span>
-          <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-white sm:mt-0">
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-white sm:mt-0">
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
                 About
               </a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
+              <a href="#" className="mr-4 hover:underline md:mr-6">
                 Privacy Policy
               </a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
+              <a href="#" className="mr-4 hover:underline md:mr-6">
                 Licensing
               </a>
             </li>
             <li>
-              <a href="#" class="hover:underline">
+              <a href="#" className="hover:underline">
                 Contact
               </a>
             </li>
