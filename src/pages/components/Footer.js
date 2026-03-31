d481f719ca5eab37591f500a19b28d04fafa15bb
w30r <mr.meorsyamil@gmail.com>
2026-04-01 02:26:43 +0800

    2023 to 2026 and goodnight

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
     <footer className=" bg-white rounded-lg shadow m-6 dark:bg-black">
       <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center md:flex md:items-center md:justify-end">
         <span className="text-sm text-teal-700 sm:text-center dark:text-teal-700">
-          © 2023{" "}
+          © 2026{" "}
           <a
-            href="https://google.com/"
+            href="https://developedbymeor.vercel.app"
             className="hover:underline font-semibold"
           >
             developedbymeor
